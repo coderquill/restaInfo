3bf1865460bdcfe13d62bf7749f0a3b2471106e8
coderquill <coderquill@gmail.com>
2020-06-28 13:13:22 +0530

    presentation component

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import Menu from './components/MenuComponents';
-import { DISHES } from './shared/dishes'
+import Main from './components/MainComponent';
 
-import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes : DISHES
-    };
-  }
 
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes = { this.state.dishes }/>
+      <div>
+        <Main/>
       </div>
+        
     );
   }
 }
